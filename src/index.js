b466eb09bce8b510f3a703d3ac47e33d6de5ebba
Mia5391 <mariakrasteva5391@gmail.com>
2018-05-25 00:40:37 -0400

    basic html look and cipher function with FOR loop

--- src/index.js
+++ src/index.js
@@ -1,1 +1,30 @@
-/* Acá va tu código */
+window.onload =function() {
+
+   const enAlphabetLength =26;
+   const asciiA = 65;
+   let cipheredText = "";
+   let offset = 2;
+   let originalText = "";
+
+   $("#cipher-button").click(function() {
+     originalText = $("#original-text").val();
+  $("#ciphered-text").text( originalText );
+   });
+
+  function cipher() {
+
+    for ( charIndex = 0; charIndex < originalText.length; charIndex++) {
+      let originalAsciiCode = originalText.charCodeAt(charIndex);
+      let cipheredAsciiCode = (originalAsciiCode - asciiA + offset)
+      % enAlphabetLength + asciiA;
+      let cipheredCharacter = String.fromCharCode (cipheredAsciiCode);
+      cipheredText = cipheredText + cipheredCharacter;
+
+
+    }
+    return cipheredText
+  }
+
+
+
+};
