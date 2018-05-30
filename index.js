693697ee2be698d3f4c9b04c797e99755c84b32f
Mia5391 <mariakrasteva5391@gmail.com>
2018-05-30 14:01:31 -0400

    parseInt added, cipher fixed

--- index.js
+++ index.js
@@ -3,30 +3,32 @@ window.onload =function() {
    const enAlphabetLength =26;
    const asciiA = 65;
    let originalText = "";
+   let offsetInput = "";
    let offset = 0;
    let resultingText = "";
 
    document.getElementById("cipher-button").onclick = function() {
-     offset = document.getElementById("offset-digit").value;
+     offsetInput = document.getElementById("offset-digit").value;
      originalText = document.getElementById("original-text").value;
-   document.getElementById("resulting-text").innerHTML = (cipher());
+     document.getElementById("resulting-text").innerHTML = (cipher());
    };
 
    document.getElementById("decipher-button").onclick = function() {
-     offset = document.getElementById("offset-digit").value;
+     offsetInput = document.getElementById("offset-digit").value;
      originalText = document.getElementById("original-text").value;
-   document.getElementById("resulting-text").innerHTML = (decipher());
+     document.getElementById("resulting-text").innerHTML = (decipher());
    };
 
   function cipher() {
 
+    offset = parseInt(offsetInput);
+
     for ( charIndex = 0; charIndex < originalText.length; charIndex++) {
       let originalAsciiCode = originalText.charCodeAt(charIndex);
-      let cipheredAsciiCode = (originalAsciiCode - asciiA + offset)
-      % enAlphabetLength + asciiA;
+      let originalPlusOffset = originalAsciiCode - asciiA + offset;
+      let cipheredAsciiCode = (originalPlusOffset % enAlphabetLength) + asciiA;
       let cipheredCharacter = String.fromCharCode (cipheredAsciiCode);
       resultingText = resultingText + cipheredCharacter;
-
 
     }
     return resultingText
@@ -34,10 +36,12 @@ window.onload =function() {
 
   function decipher() {
 
+    offset = parseInt(offsetInput);
+
     for ( charIndex = 0; charIndex < originalText.length; charIndex++) {
       let originalAsciiCode = originalText.charCodeAt(charIndex);
       let cipheredAsciiCode = (originalAsciiCode - asciiA - offset)
-      % enAlphabetLength + asciiA;
+        % enAlphabetLength + asciiA;
       let cipheredCharacter = String.fromCharCode (cipheredAsciiCode);
       resultingText = resultingText + cipheredCharacter;
 
