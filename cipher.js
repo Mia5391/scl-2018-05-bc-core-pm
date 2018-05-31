1174708b5d3308fdd955dae4686e1bcf295c336f
Mia5391 <mariakrasteva5391@gmail.com>
2018-05-31 00:15:21 -0400

    functions in cipher.js

--- cipher.js
+++ cipher.js
@@ -1,8 +1,32 @@
 window.cipher = {
-  encode: () => {
-    /* Acá va tu código */
+  encode: (offsetInput, originalText) => {
+    const enAlphabetLength =26;
+    const asciiA = 65;
+    let resultingText = "";
+    let offset = parseInt(offsetInput);
+
+    for ( charIndex = 0; charIndex < originalText.length; charIndex++) {
+      let originalAsciiCode = originalText.charCodeAt(charIndex);
+      let cipheredAsciiCode = (originalAsciiCode - asciiA + offset) % enAlphabetLength + asciiA;
+      let cipheredCharacter = String.fromCharCode (cipheredAsciiCode);
+      resultingText += cipheredCharacter;
+
+    }
+    return resultingText
+
   },
-  decode: () => {
-    /* Acá va tu código */
+  decode:  (offsetInput, originalText) => {
+    const enAlphabetLength =26;
+    const asciiA = 65;
+    let resultingText = "";
+    let offset = parseInt(offsetInput);
+
+    for ( charIndex = 0; charIndex < originalText.length; charIndex++) {
+      let originalAsciiCode = originalText.charCodeAt(charIndex);
+      let decipheredAsciiCode = (originalAsciiCode + asciiA - offset) % enAlphabetLength + asciiA;
+      let decipheredCharacter = String.fromCharCode (decipheredAsciiCode);
+      resultingText += decipheredCharacter;
+    }
+    return resultingText
   }
 }
--- index.js
+++ index.js
@@ -1,54 +1,33 @@
 window.onload =function() {
 
-   const enAlphabetLength =26;
-   const asciiA = 65;
    let originalText = "";
    let offsetInput = "";
-   let offset = 0;
-   let resultingText = "";
+
 
    document.getElementById("cipher-button").onclick = function() {
      offsetInput = document.getElementById("offset-digit").value;
-     originalText = document.getElementById("original-text").value;
-     document.getElementById("resulting-text").innerHTML = (cipher());
+     if (isInteger(offsetInput)) {
+       originalText = document.getElementById("original-text").value;
+       document.getElementById("resulting-text").innerHTML = (cipher.encode(offsetInput, originalText));
+     } else {
+       document.getElementById("resulting-text").innerHTML = "Error";
+     }
    };
 
    document.getElementById("decipher-button").onclick = function() {
      offsetInput = document.getElementById("offset-digit").value;
-     originalText = document.getElementById("original-text").value;
-     document.getElementById("resulting-text").innerHTML = (decipher());
+     if (isInteger(offsetInput)) {
+       originalText = document.getElementById("original-text").value;
+       document.getElementById("resulting-text").innerHTML = (cipher.decode(offsetInput, originalText));
+     } else {
+       document.getElementById("resulting-text").innerHTML = "Error";
+     }
    };
 
-  function cipher() {
-
-    offset = parseInt(offsetInput);
-
-    for ( charIndex = 0; charIndex < originalText.length; charIndex++) {
-      let originalAsciiCode = originalText.charCodeAt(charIndex);
-      let originalPlusOffset = originalAsciiCode - asciiA + offset;
-      let cipheredAsciiCode = (originalPlusOffset % enAlphabetLength) + asciiA;
-      let cipheredCharacter = String.fromCharCode (cipheredAsciiCode);
-      resultingText = resultingText + cipheredCharacter;
-
-    }
-    return resultingText
-  }
-
-  function decipher() {
-
-    offset = parseInt(offsetInput);
-
-    for ( charIndex = 0; charIndex < originalText.length; charIndex++) {
-      let originalAsciiCode = originalText.charCodeAt(charIndex);
-      let cipheredAsciiCode = (originalAsciiCode - asciiA - offset)
-        % enAlphabetLength + asciiA;
-      let cipheredCharacter = String.fromCharCode (cipheredAsciiCode);
-      resultingText = resultingText + cipheredCharacter;
-
-
-    }
-    return resultingText
-  }
-
+   function isInteger(offset){
+     let inputIsANumber = parseInt(offsetInput) != NaN;
+     let inputIsAnInteger = offset % 1 === 0;
+     return inputIsANumber && inputIsAnInteger;
+   }
 
 };
